d38d7e4c9bd30aa3d914d36633707c860a1757d8
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2017-05-14 20:51:39 +0530

    enable linting

--- benchmarks/promise-creation.js
+++ benchmarks/promise-creation.js
@@ -7,27 +7,29 @@ const suite = new Benchmark.Suite('Benchmarking Promise Creation');
 
 
 suite
-  .add('Native ES2015', TestPromise)
-  .add('Light Weight ', TestLightWeightPromise)
-  .add('BlueBird     ', TestBlueBirdPromise)
-  .on('start', (evt) => console.log(`${evt.currentTarget.name}...`))
-  .on('cycle', (evt) => console.log(String(evt.target)))
-  .on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-  })
-  .run({ 'async': true });
+    .add('Native ES2015', TestPromise)
+    .add('Light Weight ', TestLightWeightPromise)
+    .add('BlueBird     ', TestBlueBirdPromise)
+    .on('start', (evt) => console.log(`${evt.currentTarget.name}...`))
+    .on('cycle', (evt) => console.log(String(evt.target)))
+    .on('complete', function () {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    .run({
+        'async': true
+    });
 
 
 function TestPromise() {
-  new Promise(() => {});
+    new Promise(() => {});
 }
 
 
 function TestLightWeightPromise() {
-  LightWeightPromise.createPromise();
+    LightWeightPromise.createPromise();
 }
 
 
 function TestBlueBirdPromise() {
-  new BlueBird(() => {});
+    new BlueBird(() => {});
 }
